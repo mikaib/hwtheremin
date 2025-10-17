b773d237e97c0813f7159f38daf6101885cbc3b9
http80 <mikaib@duck.com>
2025-10-17 22:08:19 +0200

    distance outline

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 
 float g_volume = 0.0;
+float g_distance_cm = 0.0;
 
 // get the volume as a scalar (0.0 - 1.0)
 float get_volume() {
@@ -10,6 +11,16 @@ float get_volume() {
 // set the volume scalar (0.0 - 1.0)
 void set_volume(float scalar) {
   g_volume = scalar;
+}
+
+// signal sensor to read the distance
+void read_distance() {
+  g_distance_cm = 0.0; // TODO: impl
+}
+
+// read the value of the distance sensor in cm
+float get_read_distance() {
+  return g_distance_cm;
 }
 
 int main() {
