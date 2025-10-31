c9a480400bc853502f625888169d4dd64182a240
Mikai B <prod7626@gmail.com>
2025-10-31 13:35:48 +0100

    cleanup filter

---
+++ src/filter.h
@@ -0,0 +1,27 @@
+#ifndef FILTER_H
+#define FILTER_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+// enum to represent the state of the 2 buttons
+typedef enum filter_selector_state {
+  NONE,
+  UP,
+  DOWN
+}
+filter_selector_state_t;
+
+// struct for the ds
+typedef struct filter_entry {
+  uint8_t age;
+  float value;
+} filter_entry_t;
+
+int filter_compare(const void* a, const void* b);
+float get_filtered_distance();
+bool set_filter_size(int size);
+void clear_filter();
+void push_filter_value(float dist_sample);
+
+#endif // FILTER_H
